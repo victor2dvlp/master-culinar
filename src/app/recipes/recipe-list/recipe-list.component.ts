f41b9680fd1a483e0aacc3a8232d84e3122f6b29
victor2dvlp <victor_sp@ukr.net>
2018-01-20 16:03:25 +0200

    onfigured project structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RecipeModel} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recepies: RecipeModel[] = [
+    new RecipeModel('Salate', 'test salate description', 'http://www.sushiojirestaurant.de/WebRoot/Store4/Shops/78239554/MediaGallery/Bingoel-Kebap-salat1.jpg'),
+    new RecipeModel('Pizza', 'test pizza description', 'http://pizza-piemonte.com.ua/images/pizza.jpg'),
+    new RecipeModel('Pasta carbonara', 'test pastacarbonara  description', 'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18173700/051092056-01-spaghetti-carbonara-recipe-main.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
